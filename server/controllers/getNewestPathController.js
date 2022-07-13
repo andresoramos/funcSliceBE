49256fbba6f24be4f16304a5b2a852f013354017
Andres Ramos <andresoramos@gmail.com>
2022-07-14 00:03:31 +1000

    Newest controller added

--- server/controllers/getNewestPathController.js
+++ server/controllers/getNewestPathController.js
@@ -11,6 +11,18 @@ const getNewestPathController = async (req, res) =>{
         if(!pathData.length){
             return res.send({newPathName: "Path 1"});
         }
+        let highestPath = "Path 1";
+        pathData.forEach((item)=>{
+            let {table_name} = item;
+            if(table_name.split(" ")[0] === "Path"){
+                let presentNum = Number(highestPath.split(" ")[1]);
+                let itemNum = Number(table_name.split(" ")[1]);
+                if(itemNum > presentNum){
+                    highestPath = `Path ${itemNum}`
+                }
+            }
+        })
+        return res.send({highestPath: `Path ${Number(highestPath.split(" ")[1]) + 1}`})
     } catch (error) {
         res.send({dataNotSent: true, error: error.message}) 
     }
