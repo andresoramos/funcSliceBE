e018e624d77a1016c6bf8eb001ed17a14de096bc
Andres Ramos <andresoramos@gmail.com>
2022-07-22 23:11:34 +1000

    finit

--- server/controllers/runListController.js
+++ server/controllers/runListController.js
@@ -1,9 +1,5 @@
-const connection = require('../db/dbConnection');
-const {queryMap} = require('../models/queryMap');
 const {runListModel} = require("../models/motionModels");
 const {isUndefined} = require('../helpers/controllerHelpers')
-const {createTableModel} = require("../models/dataOperationsModel");
-const {saveListPoints, assignListToOwner} = require("../helpers/controllerHelpers")
 
 const runListController = async (req, res) =>{
     const {body} = req;
@@ -17,7 +13,6 @@ const runListController = async (req, res) =>{
             throw new Error(runList.error.message);
         };
         return res.send({listRan: true});
-        // await connection.query(queryMap.createPathTable(name));
         
     } catch (error) {
         res.send({runFailure: true, error: error.message})
